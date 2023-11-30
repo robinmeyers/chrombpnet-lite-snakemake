03da6f5db2d91782281dfa98c489f6fa2031d178
Robin Meyers <robin.m.meyers@gmail.com>
2023-11-30 10:36:11 -0800

    fix counts file

--- scripts/make_predict_json.py
+++ scripts/make_predict_json.py
@@ -7,7 +7,7 @@ with open(snakemake.input.template, "r") as infile:
 	parameters = json.load(infile)
 
 parameters['profile_filename'] = "outs/{sample}/fold{fold}/{sample}.fold{fold}.profile.npz".format(sample = snakemake.wildcards.sample, fold = snakemake.wildcards.fold)
-parameters['counts_filename'] = "outs/{sample}/fold{fold}/{sample}.fold{fold}.profile.npz".format(sample = snakemake.wildcards.sample, fold = snakemake.wildcards.fold)
+parameters['counts_filename'] = "outs/{sample}/fold{fold}/{sample}.fold{fold}.counts.npz".format(sample = snakemake.wildcards.sample, fold = snakemake.wildcards.fold)
 parameters['loci'] = snakemake.config['peaks']
 parameters['sequences'] = snakemake.config['genome_fa']
 parameters['chroms'] = split['test']
